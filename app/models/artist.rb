1470c319c8e5b766780cfe6faa608f67ca0e0913
Ahmed Abououf <aabuauf@yahoo.com>
2019-09-01 13:03:33 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,16 +1,37 @@
+require 'pry'
+
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs 
   
   
   
-  def slugify
-    newarray =[]
-   self.name.split(" ").each do|name|
-      newarray << name.downcase
-   end
-   
-   self.slug = newarray.join("-")
-  end 
+  # def slug
+  #   newarray =[]
+  # self.name.split(" ").each do|name|
+  #     newarray << name.downcase
+  # end
+  # newarray = newarray.join("-")
+  #   self.slug = newarray
+  #   write_attribute(:slug, newarray)
+  # end 
   
+  # def slug
+  # newarray =[]
+
+  #   read_attribute(:name).split(" ").map do|name|
+  #     name.downcase
+  #   end.join("-")
+  # end
+  
+def name=(name)
+      newarray =[]
+  name.split(" ").each do|eachname|
+      newarray << eachname.downcase
+  end
+   write_attribute(:name, name)
+    newarray = newarray.join("-") 
+  write_attribute(:slug, newarray)
+  self.save
+end
 end
