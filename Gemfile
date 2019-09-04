196d7ef30e341861cd33f7fdb32e4eee05650cc8
Ahmed Abououf <aabuauf@yahoo.com>
2019-09-04 10:50:01 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
 gem 'sinatra'
-gem 'activerecord', :require => 'active_record'
+gem 'activerecord', '~> 5.2.3', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
 gem 'require_all'
-gem 'sqlite3', '~> 1.3.6'
+gem "sqlite3"
 gem 'thin'
 gem 'shotgun'
 gem 'pry' 
@@ -15,5 +15,5 @@ group :test do
   gem 'rspec'
   gem 'capybara'
   gem 'rack-test'
-  gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
+  gem 'database_cleaner'
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
@@ -23,35 +25,63 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/songs/new' do
-    
+    @genre=Genre.all
+ 
     erb :'/songs/new' 
   end
   
   post '/songs/new' do
-    binding.pry
-    @song = Song.create(params[:name])
+    @song = Song.create(name: params[:song_name])
+    if Artist.find_by_name(params[:Artist_name])==nil
+        @artist=Artist.create(name: params[:Artist_name])
+        @song.artist = @artist
+        @song.save
+    else
+      @song.artist = Artist.find_by_name(params[:Artist_name])
+      @song.save
+    end
+    @genresArray = Genre.find_by_id(params[:genres][0])
+    @song.genres << @genresArray
     
-    binding.pry
+    redirect "/songs/#{@song.slug}" 
   end
-  get '/songs/:slug' do
-    
-    @song = Song.find_by_slug(params[:slug])
 
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @songGenres = @song.genres
     erb :show
   end
 
   get '/artists/:slug' do
-    
     @artist = Artist.find_by_slug(params[:slug])
-
     erb :'/artists/show'
   end
   
   
   get '/genres/:slug' do
-    
     @genre = Genre.find_by_slug(params[:slug])
-
     erb :'/genres/show'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @genre=Genre.all
+    erb :'/songs/edit'
+  end
+
+  patch '/songs/edit' do
+    @song = Song.find_by_name(params[:song_name])
+    if params[:Artist_name] != ""
+      binding.pry
+      @artist=Artist.create(name: params[:Artist_name])
+      @song.update(artist: @artist)
+    end
+    if params[:genres]
+      @song.genres.clear
+      @genresArray = Genre.find_by_id(params[:genres][0])
+      @song.genres << @genresArray
+    end
+    @song.save
+    redirect "/songs/#{@song.slug}" 
   end
 end
